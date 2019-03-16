05cc73025df2dfd183ca2069f957a7f7c6a41fbd
malwaredllc <danielvegamyhre@gmail.com>
2019-03-16 08:54:12 -0600

    Added selection sorting algorithm

---
+++ sorting_algorithms/selection_sort.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[] = {2, 5, 1, 6, 4, 8, 3};
+    int len = sizeof(arr) / sizeof(int);
+    
+    // Selection sort
+    for (int i = 0; i < len - 1; i++)
+    {
+        int x = arr[i];
+        int y = arr[i + 1];
+        int k = i + 1;
+        
+        for (int j = i + 1; j < len; j++)
+        {
+            if (arr[j] < y)
+            {
+                y = arr[j];
+                k = j;
+            }
+        }
+
+        if (y < x)
+        {
+            arr[i] = y;
+            arr[k] = x;
+        }
+    }
+
+    // Dislay result and exit cleanly
+    for (int n = 0; n < len; n++)
+    {
+        printf("%i ", arr[n]);
+    }
+    printf("\n");
+    return 0;
+}
+
+

609a0a21a049a9f0226aef3009f87fba7aaff224
malwaredllc <danielvegamyhre@gmail.com>
2019-04-13 08:49:44 -0600

    Added survey to PSET7

--- pset7/survey/application.py
+++ pset7/survey/application.py
@@ -31,9 +31,24 @@ def get_form():
 
 @app.route("/form", methods=["POST"])
 def post_form():
-    return render_template("error.html", message="TODO")
+    # get information from POST request and check for error
+    if not request.form.get('name') or not request.form.get('house') or not request.form.get('position'):
+        return render_template("error.html", message="Error: please complete the form")
+    name = request.form.get('name')
+    house = request.form.get('house')
+    position = request.form.get('position')
+
+    # write information to CSV file and redirect user to sheet.html
+    with open('survey.csv','a') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow([name, house, position])
+    return redirect("/sheet")
 
 
 @app.route("/sheet", methods=["GET"])
 def get_sheet():
-    return render_template("error.html", message="TODO")
+    # read student information from CSV
+    with open('survey.csv','r') as file:
+        csv_reader = csv.reader(file)
+        students = list(csv_reader)
+    return render_template("sheet.html", students=students)
